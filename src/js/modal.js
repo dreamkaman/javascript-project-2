6f3010939619748762d2fe2c7f960dd6f2583c28
levchenko1986 <vadimleva1986@gmail.com>
2021-12-02 12:55:08 +0200

    Modal closest

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,31 +5,40 @@ import modalTemplate from '../template/modal.hbs';
 
     const refs = {
       openModalBtn: document.querySelector('[data-card-photo]'),
-      closeModalBtn: document.querySelector('[data-close]'),
       modal: document.querySelector('#modal'),
+      backdropBackground: document.querySelector('#backdrop'),
     };
   
     const apiService = new ApiService();
-    
+    refs.backdropBackground.addEventListener('click', toggleModalBackdrop);
     refs.openModalBtn.addEventListener('click', toggleModal);
-    // refs.closeModalBtn.addEventListener('click', toggleModal);
+    // refs.closeModalBtn.addEventListener('click', toggleModalClose);
     const filmTempate  = film => {
         console.log("ac", film);
-        modal.innerHTML = modalTemplate(film);
+        refs.modal.innerHTML = modalTemplate(film);
     }
-    // modal.addEventListener('click', e => {
-    //     e.preventDefault();
-    //     if (e.target.className !== '') {
-    //         console.log("ac");
-    //         return;
-    //     }
-      
-    // });
+
+    function toggleModalBackdrop (e) {
+      if(e.target.classList.contains('backdrop')) {
+        refs.backdropBackground.classList.toggle('is-hidden');
+      }
+    }
+
     function toggleModal() {
-        refs.modal.classList.toggle('is-hidden');
+        refs.backdropBackground.classList.toggle('is-hidden');
         apiService.fetchFilmId(1).then(data => {
         console.log('fetchFilmId(1) - ',data)
         filmTempate(data);
+        const refs = {
+          closeModalBtn: document.querySelector('.modal-close-btn'),
+          backdropBackground: document.querySelector('#backdrop'),
+        };
+        refs.closeModalBtn.addEventListener('click', (e) => {
+          refs.backdropBackground.classList.toggle('is-hidden');
+        });  
         })
         console.log('active');
       }
+
+     
+      
